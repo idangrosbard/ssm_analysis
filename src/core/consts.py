7093fbfce589141526ee4b9e6d99b86bfa33f177
nirendy <nirendy@gmail.com>
2025-03-26 22:03:30 +0200

    update tests

--- src/core/consts.py
+++ src/core/consts.py
@@ -20,7 +20,7 @@ from src.core.types import (
 from src.utils.infra.output_path import OutputKey
 from src.utils.infra.slurm import SLURM_GPU_TYPE
 
-# os.umask(0o002)  # Set umask to 0o002 which is -rwxr-x---
+os.umask(0o002)  # Set umask to 0o002 which is -rwxr-x---
 
 
 class C_ACTIVE_USERS(StrEnum):
